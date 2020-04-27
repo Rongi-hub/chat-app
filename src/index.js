b8873a5fd83ac7c39da9eab3c7b6108bcca8e82d
Jae <cjryeong@gmail.com>
2020-04-27 16:06:02 +0300

    firebase

--- src/index.js
+++ src/index.js
@@ -6,19 +6,41 @@ import Login from './components/Login';
 import Register from './components/Register';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import firebase, {auth, provider} from './firebase.js';
 
 class AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={user:null}
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if(user){
+        this.setState({user});
+      }
+    })
+  }
+  logOutUser = () =>{
+    firebase.auth().signOut()
+    .then(window.location = "/");
+  }
   render() {
     return(
       <Router>
         <div className="app">
           <nav className="main-nav">
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </nav>
+            {!this.state.user &&
+              <div>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+              </div>
+            }
+            {this.state.user &&
+              <a href="#!" onClick={this.logOutUser}>logout</a>
+            }
+            </nav>
           <Switch>
-            <Route path="/" exact component={App} />
+            <Route path="/" exact component={() => <App user={this.state.user}/>} />
             <Route path="/login" exact component={Login} />
             <Route path="/register" exact component={Register} />
           </Switch>
